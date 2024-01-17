8c1e1a2ee0238b43d5129617bc8afae1290d819e
Lydia Young <lydsjoyy@gmail.com>
2024-01-17 17:15:18 +0000

    fix code

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path("", views.PostList.as_view(), name='home'),
-    path('<int:event_id>/', views.post_detail, name='post_detail'),
+    path('', views.post_detail, name='post_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -4,12 +4,10 @@ from .models import Post
 
 # Create your views here.
 
-
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1)
     template_name = "blog/index.html"
     paginate_by = 6
-
 
 def post_detail(request, slug):
     """
